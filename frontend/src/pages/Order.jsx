dee356fb5da9df829b92080c8e04fd17a335c8ac
Aashish_Rimal <aashishrimal2003@gmail.com>
2025-11-11 22:27:21 +0545

    update

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -86,8 +86,8 @@ const Order = () => {
           orderId: res.data.order._id,
           total_amount: total,
           product_code: "EPAYTEST",
-          success_url: "https://your-frontend-name.vercel.app/payment-success",
-          failure_url: "https://your-frontend-name.vercel.app/payment-failed"
+          success_url: "https://frontend-name.vercel.app/payment-success",
+          failure_url: "https://frontend-name.vercel.app/payment-failed"
         });
         document.write(payRes.data);
         return;
