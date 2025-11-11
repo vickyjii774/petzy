1f6f923133c9f357a168be192005094b4bfba49f
Aashish_Rimal <aashishrimal2003@gmail.com>
2025-11-11 20:54:49 +0545

    delete

--- backend/routes/admin.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const adminController = require("../controller/admin.controller");
-
-router.post("/login", adminController.loginAdmin);
-router.get("/dashboard", adminController.getDashboardStats);
-
-module.exports = router;
